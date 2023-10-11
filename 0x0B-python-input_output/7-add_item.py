593cb9126e4e08a3af718a02231beebd9e66599f
Hichammayat <hichammayat07@gmail.com>
2023-10-11 05:02:31 -0700

    commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+# -*- coding: utf-8 -*-
+"""
+This is 7-add_item module
+"""
 import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file =__import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+try:
+    new_list = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    new_list = []
+
+for i in range(1, len(sys.argv)):
+    new_list.append(sys.argv[i])
+
+try:
+    save_to_json_file(new_list, 'add_item.json')
+except IOError:
+    pass
