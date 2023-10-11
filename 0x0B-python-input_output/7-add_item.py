31b0df7a76d1e9fe2e758ff45f3ebf4eac639bda
Hichammayat <hichammayat07@gmail.com>
2023-10-11 05:11:06 -0700

    commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,16 +7,13 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+arglist = list(sys.argv[1:])
 
 try:
-    new_list = load_from_json_file('add_item.json')
-except FileNotFoundError:
-    new_list = []
+   new_list = load_from_json_file('add_item.json')
+except Exception:
+   new_list = []
 
-for i in range(1, len(sys.argv)):
-    new_list.append(sys.argv[i])
+new_list.extend(arglist)
+save_to_json_file(new_list, 'add_item.json')
 
-try:
-    save_to_json_file(new_list, 'add_item.json')
-except IOError:
-    pass
